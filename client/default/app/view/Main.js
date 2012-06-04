d5bc2bb2505d4c66dcc6d843c066765fdba064cd
Raghav Janamanchi <rjanamanchi@vmware.com>
2012-06-04 14:56:48 +0530

    Changing to Login

--- client/default/app/view/Main.js
+++ client/default/app/view/Main.js
@@ -1,45 +1,21 @@
 Ext.define("Sencha1.0.view.Main", {
     extend: 'Ext.tab.Panel',
-    requires: ['Ext.TitleBar'],
-    
-    config: {
-        tabBarPosition: 'top',
         
-        items: [
-            {
-                title: 'Welcome1',
-                iconCls: 'home',
-                
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
-                },
-                
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
-            },
-            {
-                title: 'Get Started',
-                iconCls: 'action',
-                
-                items: {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started2'
-                    },
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
+    config: {
+        refs: {
+            loginForm: 'formpanel'
+        },
+        control: {
+            'formpanel button': {
+                tap: 'doLogin'
             }
-        ]
+        }
+    },
+
+    doLogin: function() {
+        var form   = this.getLoginForm(),
+            values = form.getValues();
+
+        Sencha1.0.authenticate(values);
     }
 });
