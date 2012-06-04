895c840581a8c5a0cd608948f723dde693d130a0
Raghav Janamanchi <rjanamanchi@vmware.com>
2012-06-04 16:16:15 +0530

    Changing to Login Form

--- client/default/app.js
+++ client/default/app.js
@@ -30,7 +30,7 @@ Ext.application({
         Ext.fly('appLoadingIndicator').destroy();
 
         // Initialize the main view
-        Ext.Viewport.add(Ext.create('Sencha1.0.view.Main'));
+        Ext.Viewport.add(Ext.create('Sencha1.0.view.LoginForm'));
     },
 
     onUpdated: function() {
